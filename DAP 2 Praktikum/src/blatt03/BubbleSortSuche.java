5496c78abdbf8ac47a5cb26f09bb2fc5fceef52b
batuh <batuh@DESKTOP-N29S039>
2020-09-04 17:04:12 +0300

    Began the ternary search method

--- DAP 2 Praktikum/src/blatt03/BubbleSortSuche.java
+++ DAP 2 Praktikum/src/blatt03/BubbleSortSuche.java
@@ -1,5 +1,9 @@
 package blatt03;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class BubbleSortSuche {
 	
 	
@@ -27,6 +31,22 @@ public class BubbleSortSuche {
 		
 	}
 
+	
+	public static void ternary(int l,int r,double key,LinkedHashMap<Integer,Double> list) {
+		
+		
+		int mid1 = l+(r-l)/3;
+		int mid2 = r-(r-l)/3;
+		
+		// to be continued
+	
+	}
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		
 		float sekunden = 0;
@@ -61,6 +81,31 @@ public class BubbleSortSuche {
 		}
 		
 		
-	}
+		//the last two array sizes
+		
+		int size1 = size / 2;
+		int size2 = size1 / 2;
+		
+		System.out.println(size1+" "+size2);
+		
+		Map<Integer,Double> seconds = new LinkedHashMap<Integer,Double>();
+		
+		for(int i = size2; i <= size1; i = i+1000) {
+			
+			int[] arr = new int[i];
+			BubbleSort.rand(arr);
+			seconds.put(i,bubbleSort(arr));
+			
+		}
+		
+		
+		//ternary search
+		
+		
+		
+		
+		
+	}// end main
+	
 
 }
